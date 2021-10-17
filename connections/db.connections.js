b8e5b6f37ab4d255a7e276f09fd4d964bdf37191
Shivansh <shivansh.verma8080@gmail.com>
2021-10-17 12:28:50 +0530

    AWS configs added

--- connections/db.connections.js
+++ connections/db.connections.js
@@ -3,10 +3,12 @@ const Sequelize = require("sequelize");
 
 const ToDoModel = require("../ToDo/todo.model.js");
 
+console.log(dbConfig["awsConfig "]);
+
 const sequelize = new Sequelize( dbConfig["awsConfig "].database , dbConfig["awsConfig "].username , dbConfig["awsConfig "].password ,{
     host: dbConfig["awsConfig "].host,
     dialect: dbConfig["awsConfig "].dialect,
-    operatorsAliases: 0,
+    operatorsAliases: 0
 });
 
 const ToDo = ToDoModel(sequelize,Sequelize);
